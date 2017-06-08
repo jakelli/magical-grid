ba962e33bccc90305356f93e76218dccef1b1f8d
Jake Ellious <j_ellious@yahoo.com>
2017-06-07 19:28:28 -0500

    Added animations

--- MagicalGrid/ViewController.swift
+++ MagicalGrid/ViewController.swift
@@ -14,10 +14,8 @@ class ViewController: UIViewController {
     
     var cells = [String: UIView]()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         let width = view.frame.width / CGFloat(numViewPerRow)
         
@@ -34,23 +32,40 @@ class ViewController: UIViewController {
                 cells[key] = cellView
             }
         }
-        
         view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
     }
     
+    var selectedCell: UIView?
+    
     func handlePan(gesture: UIPanGestureRecognizer) {
         let location = gesture.location(in: view)
-        
         let width = view.frame.width / CGFloat(numViewPerRow)
-        
         let i = Int(location.x / width)
         let j = Int(location.y / width)
         print(i, j)
         
         let key = "\(i)|\(j)"
-        let cellView = cells[key]
-        cellView?.backgroundColor = .white
+        guard let cellView = cells[key] else { return }
         
+        if selectedCell != cellView {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.selectedCell?.layer.transform = CATransform3DIdentity
+            }, completion: nil)
+        }
+        
+        selectedCell = cellView
+        
+        view.bringSubview(toFront: cellView)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            cellView.layer.transform = CATransform3DMakeScale(3, 3, 3)
+        }, completion: nil)
+        
+        if gesture.state == .ended {
+            UIView.animate(withDuration: 0.5, delay: 0.25, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                self.selectedCell?.layer.transform = CATransform3DIdentity
+            }, completion:nil)
+        }
     }
     
     
@@ -61,7 +76,5 @@ class ViewController: UIViewController {
         
         return UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
-    
-    
 }
 
